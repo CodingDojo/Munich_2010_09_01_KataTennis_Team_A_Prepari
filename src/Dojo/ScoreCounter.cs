98ad881c891efabb1a6a988b6694c0297e35de94
Christian Deger <christian@deger.eu>
2010-09-01 19:48:05 +0200

    autocommit after successful build

--- src/Dojo/ScoreCounter.cs
+++ src/Dojo/ScoreCounter.cs
@@ -1,5 +1,4 @@
 using System;
-using NUnit.Framework;
 
 namespace Dojo
 {
@@ -16,69 +15,7 @@ namespace Dojo
 
         public void ScoreB()
         {
-            PointsB = 1;
-        }
-    }
-
-    [TestFixture]
-    public class ScoreCounterTests
-    {
-        [Test]
-        public void GivenANewlyCreatedScoureCounter_PlayerAHasZeroPoints()
-        {
-            var scoreCounter = new ScoreCounter();
-
-            Assert.AreEqual(0, scoreCounter.PointsA);
-        }
-
-        [Test]
-        public void GivenANewlyCreatedScoureCounter_PlayerBHasZeroPoints()
-        {
-            var scoreCounter = new ScoreCounter();
-
-            Assert.AreEqual(0, scoreCounter.PointsB);
-        }
-
-        [Test]
-        public void GivenANewlyCreatedScoreCounter_AndPlayerAScores_PlayerAHasOnePoint()
-        {
-            var scoreCounter = new ScoreCounter();
-
-            scoreCounter.ScoreA();
-
-            Assert.AreEqual(1, scoreCounter.PointsA);
-        }
-
-        [Test]
-        public void GivenANewlyCreatedScoreCounter_AndPlayerBScores_PlayerBHasOnePoint()
-        {
-            var scoreCounter = new ScoreCounter();
-
-            scoreCounter.ScoreB();
-
-            Assert.AreEqual(1, scoreCounter.PointsB);
-        }
-
-        [Test]
-        public void GivenAScoreCounterWherePlayerAHasOnePoint_AndPlayerAScore_PlayerAHasTwoPoints()
-        {
-            var scoreCounter = new ScoreCounter();
-            scoreCounter.ScoreA();
-
-            scoreCounter.ScoreA();
-
-            Assert.AreEqual(2, scoreCounter.PointsA);
-        }
-
-        [Test]
-        public void GivenAScoreCounterWherePlayerBHasOnePoint_AndPlayerBScores_PlayerBHasTwoPoints()
-        {
-            var scoreCounter = new ScoreCounter();
-            scoreCounter.ScoreB();
-
-            scoreCounter.ScoreB();
-
-            Assert.AreEqual(2, scoreCounter.PointsB);
+            PointsB++;
         }
     }
 }
---
+++ src/Dojo/ScoreCounterTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+namespace Dojo
+{
+    [TestFixture]
+    public class ScoreCounterTests
+    {
+        [Test]
+        public void GivenANewlyCreatedScoureCounter_PlayerAHasZeroPoints()
+        {
+            var scoreCounter = new ScoreCounter();
+
+            Assert.AreEqual(0, scoreCounter.PointsA);
+        }
+
+        [Test]
+        public void GivenANewlyCreatedScoureCounter_PlayerBHasZeroPoints()
+        {
+            var scoreCounter = new ScoreCounter();
+
+            Assert.AreEqual(0, scoreCounter.PointsB);
+        }
+
+        [Test]
+        public void GivenANewlyCreatedScoreCounter_AndPlayerAScores_PlayerAHasOnePoint()
+        {
+            var scoreCounter = new ScoreCounter();
+
+            scoreCounter.ScoreA();
+
+            Assert.AreEqual(1, scoreCounter.PointsA);
+        }
+
+        [Test]
+        public void GivenANewlyCreatedScoreCounter_AndPlayerBScores_PlayerBHasOnePoint()
+        {
+            var scoreCounter = new ScoreCounter();
+
+            scoreCounter.ScoreB();
+
+            Assert.AreEqual(1, scoreCounter.PointsB);
+        }
+
+        [Test]
+        public void GivenAScoreCounterWherePlayerAHasOnePoint_AndPlayerAScores_PlayerAHasTwoPoints()
+        {
+            var scoreCounter = new ScoreCounter();
+            scoreCounter.ScoreA();
+
+            scoreCounter.ScoreA();
+
+            Assert.AreEqual(2, scoreCounter.PointsA);
+        }
+
+        [Test]
+        public void GivenAScoreCounterWherePlayerBHasOnePoint_AndPlayerBScores_PlayerBHasTwoPoints()
+        {
+            var scoreCounter = new ScoreCounter();
+            scoreCounter.ScoreB();
+
+            scoreCounter.ScoreB();
+
+            Assert.AreEqual(2, scoreCounter.PointsB);
+        }
+    }
+}
