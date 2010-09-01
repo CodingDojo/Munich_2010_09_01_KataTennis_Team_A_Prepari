525c4d475e630261953446abe90d355d60e66940
Christian Deger <christian@deger.eu>
2010-09-01 22:08:10 +0200

    autocommit after successful build

--- src/Dojo/ScoringEngineTests.cs
+++ src/Dojo/ScoringEngineTests.cs
@@ -1,4 +1,3 @@
-using System;
 using NUnit.Framework;
 
 namespace Dojo
@@ -136,71 +135,6 @@ namespace Dojo
             string score = scoringEngine.Score;
 
             Assert.AreEqual("love-fifteen", score);
-        }
-    }
-
-    public class ScoringEngine
-    {
-        private readonly IScore _scoreCounter;
-
-        public ScoringEngine(IScore scoreCounter)
-        {
-            _scoreCounter = scoreCounter;
-        }
-
-        public string Score
-        {
-            get
-            {
-                if (_scoreCounter.PointsA >= 3 && _scoreCounter.PointsB >= 3)
-                {
-                    if (_scoreCounter.PointsA == _scoreCounter.PointsB)
-                    {
-                        return "deuce";
-                    }
-
-                    if (_scoreCounter.PointsA - _scoreCounter.PointsB == 1)
-                    {
-                        return "Advantage PlayerA";
-                    }
-
-                    if (_scoreCounter.PointsB - _scoreCounter.PointsA == 1)
-                    {
-                        return "Advantage PlayerB";
-                    }    
-                    
-                    if (_scoreCounter.PointsB - _scoreCounter.PointsA == 2)
-                    {
-                        return "PlayerB wins";
-                    } 
-                    
-                    if (_scoreCounter.PointsA - _scoreCounter.PointsB == 2)
-                    {
-                        return "PlayerA wins";
-                    }
-                }
-
-                string scorePhraseA = GetScorePhrase(_scoreCounter.PointsA);
-                string scorePhraseB = GetScorePhrase(_scoreCounter.PointsB);
-                return scorePhraseA + "-" + scorePhraseB;
-            }
-        }
-
-        private string GetScorePhrase(int points)
-        {
-            switch (points)
-            {
-                case 0:
-                    return "love";
-                case 1:
-                    return "fifteen";
-                case 2:
-                    return "thirty";
-                case 3:
-                    return "fourty";
-                default:
-                    throw new ArgumentException("Undefinied points");
-            }
         }
     }
 }
